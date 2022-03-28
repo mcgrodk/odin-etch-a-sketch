7e1e6ce7f439061fc4457d5c86b69dc96dfdd30b
Kyle <mcgroderk@gmail.com>
2022-03-28 16:13:08 +0900

    Added HTML, CSS, JS files with starter elements

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odin-etch-a-sketch
+An Etch-a-Sketch webpage made for the Odin Project's curriculum to practice DOM manipulation and general HTML/CSS/JavaScript skills.
---
+++ script.js
@@ -0,0 +1,11 @@
+const etchContainer = document.querySelector("#etch-container");
+const etchDiv = document.createElement('div');
+etchDiv.style.cssText = "border: 1px solid gray; height: 2rem; width: 2rem;"
+etchDiv.classList.add("etch-div");
+etchContainer.appendChild(etchDiv);
+
+
+function makeDivs() {
+
+}
+
