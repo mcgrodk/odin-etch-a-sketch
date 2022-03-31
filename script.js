da67ec8b4bc3570aa9934c1b57a7b66c40b42c1a
Kyle <mcgroderk@gmail.com>
2022-03-31 12:09:50 +0900

    Made working 16x16 etch a sketch; clear button event not working

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 const etchContainer = document.querySelector("#etch-container");
-const etchDiv = document.createElement('div');
-etchDiv.style.cssText = "border: 1px solid gray; height: 2rem; width: 2rem;"
-etchDiv.classList.add("etch-div");
-etchContainer.appendChild(etchDiv);
 
-
-function makeDivs() {
-
+for (i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    etchContainer.appendChild(div);
 }
 
+const divs = etchContainer.querySelectorAll('div');
+divs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'black';
+    });
+});
+
+const clearBtn = document.createElement('button');
+clearBtn.textContent = 'New Grid';
+document.body.appendChild(clearBtn);
+
+clearBtn.addEventListener('click', () => {
+
+    divs.forEach((div) => {
+        div.style.backgroundColor = 'white';
+    });
+
+//Event not working below this line
+    etchContainer.removeChild(divs);
+
+    let newGridNum = parseInt(prompt('How many squares per row?'));
+    
+    for(i = 0; i < (newGridNum ** 2); i++) {
+        const div = document.createElement('div');
+        etchContainer.appendChild(div);
+    }
+});
+
+
+//On button click:
+//All existing divs are cleared (so that new divs aren't created in addition to old divs)
