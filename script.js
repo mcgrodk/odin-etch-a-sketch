b24e8d87ef1efbeefe248b5755f4b20130865eff
Kyle <mcgroderk@gmail.com>
2022-04-06 15:12:05 +0900

    Fixed events, added buttons for black/rainbow/eraser modes

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
 const etchContainer = document.querySelector("#etch-container");
+const buttonContainer = document.querySelector('#button-container');
+const blackModeBtn = document.querySelector('#black-btn');
+const rainbowModeBtn = document.querySelector('#rainbow-btn');
+const eraserBtn = document.querySelector('#eraser-btn');
+const clearBtn = document.querySelector('#clear-btn');
+
+let colorMode = 'black';
 
 function makeDivs(gridSquares) {
 for (i = 0; i < (gridSquares ** 2); i++) {
     const div = document.createElement('div');
     etchContainer.appendChild(div);
-}
-  etchContainer.style.gridTemplateColumns = `repeat(${gridSquares}, auto)`;
-  etchContainer.style.gridTemplateRows = `repeat(${gridSquares}, auto)`;
+};
 };
 
-makeDivs(16);
-
-const divs = etchContainer.querySelectorAll('div');
-divs.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'black';
-    });
-});
-
-const clearBtn = document.createElement('button');
-clearBtn.textContent = 'New Grid';
-document.body.appendChild(clearBtn);
-
-clearBtn.addEventListener('click',function () {
+function formGrid(gridSquares) {
+  
+  makeDivs(gridSquares);
+  
+  etchContainer.style.gridTemplateColumns = `repeat(${gridSquares}, auto)`;
+  etchContainer.style.gridTemplateRows = `repeat(${gridSquares}, auto)`;
+  
+   const divs = etchContainer.querySelectorAll('div');
     divs.forEach((div) => {
-        etchContainer.removeChild(div);
+    div.addEventListener('mouseover', () => {
+        changeColorMode(div);
     });
-    makeDivs(parseInt(prompt('How many squares per row?')));
+});
+}
+
+function changeColorMode(div) {
+    if (colorMode == 'black') {
+        div.style.backgroundColor = 'black';
+        return;
+    } else if (colorMode == 'rainbow') {
+        randomColor = Math.floor(Math.random()*16777215).toString(16);
+        div.style.backgroundColor = '#' + randomColor;
+        return;
+    } else if (colorMode == 'eraser') {
+        div.style.backgroundColor = 'white';
+        return;
+    }
+}
+
+formGrid(16);
+
+blackModeBtn.addEventListener('click', () => {
+    colorMode = 'black';
 });
 
-// Button event makes new divs, but only works once and divs lack color-change event
+rainbowModeBtn.addEventListener('click', () => {
+    colorMode = 'rainbow';
+});
+
+eraserBtn.addEventListener('click', () => {
+    colorMode = 'eraser';
+});
+
+clearBtn.addEventListener('click', () => {
+    etchContainer.innerHTML = '';
+    let gridSquares = (parseInt(prompt('How many squares per row?')));
+    if (gridSquares < 2) gridSquares = 2;
+    if (gridSquares > 100) gridSquares = 100;
+    if (gridSquares == null) return;
+    formGrid(gridSquares);
+});
